b0ba24f7182f0102aa279c6e84b0cadee47ef643
zhivaeevvd <mass2014@yandex.ru>
2023-02-07 14:12:15 +0300

    initial commit

---
+++ SchoolStore/SchoolStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolStoreApp.swift
+//  SchoolStore
+//
+//  Created by Headdds and hands on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SchoolStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
